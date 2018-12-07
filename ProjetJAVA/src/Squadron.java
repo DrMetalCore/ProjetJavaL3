fc605443e3e4670c763a61674ad4f741b77cf7a6
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-07 11:59:21 +0100

    InvasionBox

--- ProjetJAVA/src/Squadron.java
+++ ProjetJAVA/src/Squadron.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
+
+import javafx.scene.canvas.GraphicsContext;
+import sun.font.GraphicComponent;
 
 public class Squadron {
 	private List<SpaceShip> shipsList;
 
 	public Squadron(List<SpaceShip> shipsList) {
 		this.shipsList = shipsList;
+	}
+	public Squadron() {
+		this.shipsList = new ArrayList<SpaceShip>();
+	}
+	
+	public void addSpaceShip(SpaceShip s)
+	{
+		this.shipsList.add(s);
+	}
+	
+	public void showAllSpaceShip(GraphicsContext gc)
+	{
+		for (SpaceShip spaceShip : shipsList) {
+			
+			spaceShip.getSprite().updatePosition();
+			spaceShip.getSprite().render(gc);
+		}
 	}
 	
 	
