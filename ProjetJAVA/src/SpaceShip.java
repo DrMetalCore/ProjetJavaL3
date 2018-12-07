fb5d49a0883ea6c09b08905ebb3dec5f396110d2
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-07 11:58:31 +0100

    InvasionBox

--- ProjetJAVA/src/SpaceShip.java
+++ ProjetJAVA/src/SpaceShip.java
@@ -3,7 +3,7 @@ public class SpaceShip {
 	
 	private int speed,prodTime,attak,type;
 	private Sprite sprite;
-	public SpaceShip(int speed, int prodTime, int attak, int type) {
+	public SpaceShip(int type) {
 		
 		this.type=type;
 
@@ -12,15 +12,19 @@ public class SpaceShip {
 			this.speed = 10;
 			this.prodTime = 1;
 			this.attak = 1;
-			this.sprite = new Sprite("images/broche-triangle.jpg", 18, 22, 600, 600);
+			this.sprite = new Sprite(Game.getRessourcePathByName("ressources/SpaceShipType1.png"), 18, 22, 600, 600);
 		}
 		if(this.type==2)
 		{
 			this.speed = 7;
 			this.prodTime = 5;
 			this.attak = 50;
-			this.sprite = new Sprite("images/autocollant-tableau-noir-figures-pentagone.jpg", 40, 40, 600, 600);
+			this.sprite = new Sprite(Game.getRessourcePathByName("ressources/SpaceShipType2.png"), 40, 40, 600, 600);
 		}
 	}
+	public Sprite getSprite() {
+		return sprite;
+	}
+	
 	
 }
