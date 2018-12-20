cac64632136318681181f85798493693c8222def
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-20 15:37:04 +0100

    Normalement ça marche

--- ProjetJAVA/src/Planet.java
+++ ProjetJAVA/src/Planet.java
@@ -15,6 +15,7 @@ public class Planet {
 	private Player owner;
 	private int nbOfSpaceshipToSend;
 	private boolean selectedAndValid;
+	private int squadronReady;
 	private List<Squadron> squadrons;
 	
 	public Planet(int diameter, int defencePow, int prodRate, int typeProducted, Sprite sprite, Player owner) {
@@ -27,19 +28,23 @@ public class Planet {
 		this.owner = owner;
 		this.nbOfSpaceshipToSend = 0;
 		this.selectedAndValid = false;
+		this.squadronReady = -1;
 		this.squadrons = new ArrayList<Squadron>();
 	}
 	public int getDefencePow() {
 		return defencePow;
 	}
-
-	public void setDefencePow(int defencePow) {
-		this.defencePow = defencePow;
-	}
 	public Sprite getSprite() {
 		return sprite;
 	}
 	
+	public int getSquadronReady() {
+		return squadronReady;
+	}
+	
+	protected void setSquadronReady(int squadronReady) {
+		this.squadronReady = squadronReady;
+	}
 	public List<Squadron> getSquadrons() {
 		return squadrons;
 	}
@@ -69,7 +74,10 @@ public class Planet {
 			}
 		}
 	}
-	
+	public void timeAugmentation()
+	{
+		this.defencePow++;
+	}
 	public boolean isSelected(double x, double y)
 	{
 		if(Math.sqrt(Math.pow(x-(this.getSprite().getX()+this.getSprite().width()/2), 2)+Math.pow(y-(this.getSprite().getY()+this.getSprite().width()/2), 2))<this.getSprite().width()/2) return true;
@@ -102,16 +110,30 @@ public class Planet {
 		try {
 			
 			this.nbOfSpaceshipToSend = Integer.parseInt(field.getText());
-			System.out.println(this.nbOfSpaceshipToSend);
+			if(this.nbOfSpaceshipToSend >= 0 && this.nbOfSpaceshipToSend<=100)
+			{
 			window.close();
+			int j = 0;
 			Squadron sq = new Squadron();
-			for (int i = 0; i < nbOfSpaceshipToSend; i++) {
+			for (int i = 0; i < (nbOfSpaceshipToSend*this.defencePow+18)/100; i++) {
 				SpaceShip sp = new SpaceShip(this.typeProducted);
-				sp.getSprite().setPosition(20, 20);
+				double[] spaceShipPositions = generatePointInOrbit(j*20);
+				sp.getSprite().setPosition(spaceShipPositions[0], spaceShipPositions[1]);
+				//sp.getSprite().setPosition(200, 200);
+		
 				sq.addSpaceShip(sp);
+
+				this.defencePow = this.defencePow - sp.getAttak();
+				j++;
 			}
 			this.squadrons.add(sq);
+			this.squadronReady =this.squadrons.indexOf(sq);
 			return true;
+			}
+			else {
+				Exception e = null;
+				throw e;
+			} 
 		} catch (Exception e) {
 			return false;
 		}
@@ -119,14 +141,14 @@ public class Planet {
 	public void nbOfSpaceShipBox(Stage primaryStage)
 	{
 		
-		Label secondLabel = new Label("Combien de vaisseaux voulez vous envoyer ?");
+		Label secondLabel = new Label("Combien de vaisseaux voulez vous envoyer ? (en %)");
 		TextField nbOfSpaceShip = new TextField ();
 		Button buttonV = new Button("Valider");
 
         VBox secondaryLayout = new VBox(10);
         secondaryLayout.getChildren().addAll(secondLabel,nbOfSpaceShip,buttonV);
         
-        Scene secondScene = new Scene(secondaryLayout, 300, 125);
+        Scene secondScene = new Scene(secondaryLayout, 350, 125);
         
         
         // New window (Stage)
@@ -140,5 +162,19 @@ public class Planet {
         newWindow.setY(primaryStage.getY() + 100);
 
         newWindow.show();
+	}
+	public double[] generatePointInOrbit(double angleDeg) {
+		double[] point = new double[2];
+		double angleRad = Math.toRadians(angleDeg);
+		double radius =  (this.getSprite().width())/2;
+		
+		double centerX = this.sprite.getX() + radius ;
+		double centerY = this.sprite.getY() + radius ;
+		double x = (centerX + radius * Math.cos(angleRad));
+		double y = (centerY + radius * Math.sin(angleRad));
+	
+		point[0] = x;
+		point[1] = y;
+		return point;
 	}
 }
