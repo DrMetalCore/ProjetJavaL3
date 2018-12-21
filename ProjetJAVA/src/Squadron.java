90d450e5e2d649cad1a2a2ece4cb23db1d0ccb5a
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 23:17:03 +0100

    add javadoc

--- ProjetJAVA/src/Squadron.java
+++ ProjetJAVA/src/Squadron.java
@@ -7,24 +7,14 @@ import sun.font.GraphicComponent;
 /**
  * 
  * @author Luka Moraiz and Clément Brandel
- * 
+ * @version 1
  */
 public class Squadron implements Serializable{
 	private ArrayList<SpaceShip> shipsList;
 	private Planet planetDestination;
 	/**
-	 * 
-	 * @param shipsList  list of spaceship
-	 * create a Squadron object
-	 * @version 1
-	 */
-	public Squadron(ArrayList<SpaceShip> shipsList) {
-		this.shipsList = shipsList;
-	}
-	/**
-	 * 
+	 * Create a Squadron of object with a Planet of destination
 	 * @param planetDestination  planet of destination
-	 * create a Squadron of object with a Planet of destination
 	 * @version 1
 	 */
 	public Squadron(Planet planetDestination) {
@@ -32,8 +22,8 @@ public class Squadron implements Serializable{
 		this.planetDestination = planetDestination;
 	}
 	/**
-	 * 
-	 * @param s  spaceship for add to shipsList
+	 * Add a spaceship to the squadron
+	 * @param s  spaceship to add to the shipsList
 	 * @version 1
 	 */
 	public void addSpaceShip(SpaceShip s)
@@ -41,8 +31,8 @@ public class Squadron implements Serializable{
 		this.shipsList.add(s);
 	}
 	/**
-	 * create all spaceship of squadron and they go on the planet of destination
-	 * @param gc the context graphics of the window 
+	 * Show all the spaceships of the squadron and they go on the planet of destination
+	 * @param gc the graphics context of the window 
 	 * @version 1
 	 */
 	public void showAllSpaceShip(GraphicsContext gc)
@@ -52,16 +42,6 @@ public class Squadron implements Serializable{
 			System.out.println(planetDestination);
 			
 			spaceShip.spaceShipToPlanet(spaceShip.getSprite().vectorSpaceShipToPlanet(spaceShip.vector(this.planetDestination)));
-			/*
-			for ( Planet planet : Game.getPlanetslist()) {
-				
-				if  ((planet.getSprite().distance(spaceShip.getSprite().getX(), spaceShip.getSprite().getY())) < 100) {
-					
-					spaceShip.pointRotation(planet.getSprite().getX() , planet.getSprite().getY() , 12, planet);
-					
-				}
-			}
-			*/
 			if (spaceShip.spaceShipOnPlanet(planetDestination) == true ) {
 				shipsList.remove(spaceShip);
 			}
