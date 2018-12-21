e84c630863e8fa632fd31ed6dae6044475220503
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 18:04:21 +0100

    IA and sauvegarde

--- ProjetJAVA/src/Player.java
+++ ProjetJAVA/src/Player.java
@@ -1,10 +1,12 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class Player {
-	
+public class Player implements Serializable{
+
 	private String name;
-	private List<Planet> planets = new ArrayList<Planet>();;
+	private ArrayList<Planet> planets = new ArrayList<Planet>();
 	
 	public Player(String name) {
 		this.name = name;
@@ -17,5 +19,15 @@ public class Player {
 	{
 		if(planets.contains(p)) return true;
 		else return false;
+	}
+	public void automaticAttak()
+	{
+		Random r = new Random();
+		int numMin= 0;
+		int numMax = this.planets.size();
+		int attakingPlanet = r.nextInt(numMax-numMin)+numMin;
+		int numberSpaceShip = r.nextInt(30-10)+10;
+		this.planets.get(attakingPlanet).generateSpaceShips(numberSpaceShip);
+		
 	}
 }
