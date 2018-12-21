c006c9b5a43761f6e47bc0e51ea50cdee89d5599
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 18:06:57 +0100

    IA et sauvegarde

--- ProjetJAVA/src/Squadron.java
+++ ProjetJAVA/src/Squadron.java
@@ -1,12 +1,13 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.canvas.GraphicsContext;
 import sun.font.GraphicComponent;
 
-public class Squadron {
-	private List<SpaceShip> shipsList;
-	public Squadron(List<SpaceShip> shipsList) {
+public class Squadron implements Serializable{
+	private ArrayList<SpaceShip> shipsList;
+	public Squadron(ArrayList<SpaceShip> shipsList) {
 		this.shipsList = shipsList;
 	}
 	public Squadron() {
