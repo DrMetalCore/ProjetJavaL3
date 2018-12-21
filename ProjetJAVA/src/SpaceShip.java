6989b94c25d8252a8455ae45e24398aa350be525
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 23:23:20 +0100

    add javadoc

--- ProjetJAVA/src/SpaceShip.java
+++ ProjetJAVA/src/SpaceShip.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * 
+ * Class that describe a SpaceShip
  * @author Luka Moraiz and Clément Brandel
  * @version 1
  */
@@ -10,8 +10,8 @@ public class SpaceShip implements Serializable{
 	
 	private int speed,prodTime,attak,type;
 	private Sprite sprite;
-	/** create a SpaceShip object with a type
-	* @param type     the type define some property like speed , attak , production's time and sprite.
+	/** create a SpaceShip object with the type
+	* @param type  type of the spaceship, the type define some property like speed , attak , production's time and sprite.
 	* @version 1
 	*/
 	public SpaceShip(int type) {
@@ -47,8 +47,8 @@ public class SpaceShip implements Serializable{
 		return speed;
 	}
 	/**
-	 * 
-	 * @param p   take position of the planet p     
+	 * Create a vector
+	 * @param p  Planet of destination    
 	 * @return  a vector(spaceship to planet)
 	 * @version 1
 	 */
@@ -59,17 +59,16 @@ public class SpaceShip implements Serializable{
 		return tab;
 	}
 	/**
-	 * 
+	 * Set the speed to spaceship
 	 * @param tab  tab include vector in tab[0] and tab[1]
-	 * give speed to spaceship for planet
 	 * @version 1
 	 */
 	public void spaceShipToPlanet (double[] tab) {
 		this.getSprite().setSpeed(tab[0]/3, tab[1]/3);
 	}
 	/**
-	 * 
-	 * @param planetDestination     spaceship go on planetDestination
+	 * Test if the spaceship has arrived to destination
+	 * @param planetDestination destination of the spaceship
 	 * @return true if spaceship is on the planetDestination, false otherwise
 	 * @version 1
 	 */
@@ -79,29 +78,5 @@ public class SpaceShip implements Serializable{
 		}
 		return false;
 	}
-	/**
-	 * the function do a rotation of spaceship around the planet p
-	 * center of planet  = (cx,cy) 
-	 * @param cx 
-	 * @param cy
-	 * @param angle ==> angle in degrees
-	 * @param p ==> planet
-	 * @version 1
-	 */
-	public void pointRotation(double cx,double cy, double angle, Planet p){ // centre de la planete : (cx,cy)      // angle en degré 
-		double dirP1 = Math.atan2(cy - this.getSprite().getY(), cx-this.getSprite().getX()); 
-	   
-	 	double dirP2 = dirP1 + angle; 
-	   
-	 	double dist = this.getSprite().distance(cx, cy); 
-	 	if (this.getSprite().distance(p.getSprite().getX(), p.getSprite().getY()) <  p.getSprite().distance(cx, cy)){
-	 		this.spaceShipToPlanet(this.getSprite().vectorSpaceShipToPlanet(this.vector(p)));
-	 	}
-	 	else {
-	 		/*this.getSprite().setPosition((cx + dist*Math.cos(dirP2)), (int)(cy + dist*Math.sin(dirP2)));*/
-	 		this.getSprite().setPosition((cx + dist*Math.cos(dirP2)), (int)(cy + dist*Math.sin(dirP2)));
-	 	}
-	 
-} 
 	
 }
