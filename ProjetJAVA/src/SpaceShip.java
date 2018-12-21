8958e4d808aed3d4886015daf19dc38930200aa8
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 20:30:09 +0100

    add Deplacements

--- ProjetJAVA/src/SpaceShip.java
+++ ProjetJAVA/src/SpaceShip.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class SpaceShip implements Serializable{
 	
@@ -34,6 +35,50 @@ public class SpaceShip implements Serializable{
 		return speed;
 	}
 	
+	public double[] vector(Planet p) {
+		double tab[] = {0,0};
+		tab[0]= p.getSprite().getX() - this.getSprite().getX();
+		tab[1]= p.getSprite().getY() - this.getSprite().getY();
+		return tab;
+	}
 	
+	public void spaceShipToPlanet (double[] tab) {
+		this.getSprite().setSpeed(tab[0]/3, tab[1]/3);
+	}
+	
+	public boolean spaceShipOnPlanet(Planet planetDestination) {
+		if (this.getSprite().distance(planetDestination.getSprite().getX()+planetDestination.getSprite().width(), planetDestination.getSprite().getY() + planetDestination.getSprite().height())<30) {
+			return true ;
+		}
+		return false;
+	}
+	
+	public void dodgePlanet(ArrayList<Planet> planetList) {
+		for ( Planet planet : planetList) {
+			if ((planet.getSprite().distance(this.getSprite().getX(), this.getSprite().getY())) == 100) {
+				System.out.println(planet);
+				this.getSprite().setSpeed(this.getSprite().getX() - this.getSprite().getXspeed()*Math.sin(Math.toRadians(180)), this.getSprite().getY() - this.getSprite().getYspeed()*Math.cos(Math.toRadians(180))); // cos et sinus..
+				
+			}
+		}
+		
+		
+	}// marche pas encore
+	
+	public void pointRotation(double cx,double cy, double angle, Planet p){ // centre de la planete : (cx,cy)      // angle en degré 
+		double dirP1 = Math.atan2(cy - this.getSprite().getY(), cx-this.getSprite().getX()); 
+	   
+	 	double dirP2 = dirP1 + angle; 
+	   
+	 	double dist = this.getSprite().distance(cx, cy); 
+	 	if (this.getSprite().distance(p.getSprite().getX(), p.getSprite().getY()) <  p.getSprite().distance(cx, cy)){
+	 		this.spaceShipToPlanet(this.getSprite().vectorSpaceShipToPlanet(this.vector(p)));
+	 	}
+	 	else {
+	 		/*this.getSprite().setPosition((cx + dist*Math.cos(dirP2)), (int)(cy + dist*Math.sin(dirP2)));*/
+	 		this.getSprite().setPosition((cx + dist*Math.cos(dirP2)), (int)(cy + dist*Math.sin(dirP2)));
+	 	}
+	 
+} 
 	
 }
