69c4a16462e36f5e6abbe6a53fc9fe501293e46b
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 22:51:57 +0100

    add javadoc

--- ProjetJAVA/src/Player.java
+++ ProjetJAVA/src/Player.java
@@ -2,36 +2,44 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
+/**
+ * Class that describe a player
+ * @author Luka Moraiz et Clément Brandel
+ * 
+ */
 public class Player implements Serializable{
 
 	private String name;
 	private ArrayList<Planet> planets = new ArrayList<Planet>();
+	
 	/**
-	 * 
-	 * @param name ==> player's name
+	 * Create a player object with a name
+	 * @param name player's name
 	 */
 	public Player(String name) {
 		this.name = name;
 	}
 	/**
-	 * 
-	 * @param pl ==> player's planet
+	 * Add a planet to player's planets list
+	 * @param pl planet to add
 	 */
 	public void addPlanet(Planet pl) {
 		planets.add(pl);
 	}
 	/**
-	 * 
-	 * @param p
-	 * @return
+	 * Test if the player control a planet
+	 * @param p planet to test
+	 * @return true if he control it false otherwise
 	 */
 	public boolean havePlanet(Planet p)
 	{
 		if(planets.contains(p)) return true;
 		else return false;
 	}
-	
+	/**
+	 * Allow the IA to automaticly attack an other planet
+	 * 
+	 */
 	public void automaticAttak()
 	{
 		Random r = new Random();
