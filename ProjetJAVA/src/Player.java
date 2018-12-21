50d186e85f8155ba7932b9de6bf5c31c05e36436
clementbrandel <45496971+clementbrandel@users.noreply.github.com>
2018-12-21 21:55:39 +0100

    Update Player.java

--- ProjetJAVA/src/Player.java
+++ ProjetJAVA/src/Player.java
@@ -1,21 +1,51 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class Player {
-	
+public class Player implements Serializable{
+
 	private String name;
-	private List<Planet> planets = new ArrayList<Planet>();;
-	
+	private ArrayList<Planet> planets = new ArrayList<Planet>();
+	/**
+	 * 
+	 * @param name ==> player's name
+	 */
 	public Player(String name) {
 		this.name = name;
 	}
-	
+	/**
+	 * 
+	 * @param pl ==> player's planet
+	 */
 	public void addPlanet(Planet pl) {
 		planets.add(pl);
 	}
+	/**
+	 * 
+	 * @param p
+	 * @return
+	 */
 	public boolean havePlanet(Planet p)
 	{
 		if(planets.contains(p)) return true;
 		else return false;
+	}
+	
+	public void automaticAttak()
+	{
+		Random r = new Random();
+		int numMin= 0;
+		int numMax = this.planets.size();
+		Planet attakingPlanet = this.planets.get(r.nextInt(numMax-numMin)+numMin);
+		int numberSpaceShip = r.nextInt(30-10)+10;
+		
+		numMin= 0;
+		numMax = Game.getPlanetslist().size();
+		Planet planetToAttak = Game.getPlanetslist().get(r.nextInt(numMax-numMin)+numMin);
+		while(planets.contains(planetToAttak))  planetToAttak = Game.getPlanetslist().get(r.nextInt(numMax-numMin)+numMin);
+
+		attakingPlanet.generateSpaceShips(numberSpaceShip, planetToAttak);
+		
 	}
 }
