2a666bca68ea42fbe1f09bbcf81c76b10dc68dab
clementbrandel <45496971+clementbrandel@users.noreply.github.com>
2018-12-21 16:27:09 +0100

    Update Squadron.java

--- ProjetJAVA/src/Squadron.java
+++ ProjetJAVA/src/Squadron.java
@@ -30,14 +30,14 @@ public class Squadron {
 			
 			spaceShip.spaceShipToPlanet(spaceShip.getSprite().vectorSpaceShipToPlanet(spaceShip.vector(this.planetDestination)));
 			
-			for ( Planet planet : planetsList ) {
+			/*for ( Planet planet : planetsList ) {
 				
 				if  ((planet.getSprite().distance(spaceShip.getSprite().getX(), spaceShip.getSprite().getY())) < 100) {
 					
 					spaceShip.pointRotation(planet.getSprite().getX() , planet.getSprite().getY() , 12);
 					
 				}
-			}
+			}*/
 			
 			if (spaceShip.spaceShipOnPlanet(planetDestination) == true ) {
 				shipsList.remove(spaceShip);
