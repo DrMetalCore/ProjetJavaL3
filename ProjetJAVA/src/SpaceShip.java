ab64f7bf8f794e14dae62662ff4a2489aa02eb27
DrMetalCore <33600797+DrMetalCore@users.noreply.github.com>
2018-12-21 18:05:30 +0100

    IA and sauvegarde

--- ProjetJAVA/src/SpaceShip.java
+++ ProjetJAVA/src/SpaceShip.java
@@ -1,5 +1,6 @@
+import java.io.Serializable;
 
-public class SpaceShip {
+public class SpaceShip implements Serializable{
 	
 	private int speed,prodTime,attak,type;
 	private Sprite sprite;
@@ -11,14 +12,14 @@ public class SpaceShip {
 		{
 			this.speed = 10;
 			this.prodTime = 1;
-			this.attak = 10;
+			this.attak = 1;
 			this.sprite = new Sprite(Game.getRessourcePathByName("ressources/SpaceShipType1.png"), 18, 22, Game.getHeight(), Game.getHeight());
 		}
 		if(this.type==2)
 		{
 			this.speed = 7;
 			this.prodTime = 5;
-			this.attak = 50;
+			this.attak = 5;
 			this.sprite = new Sprite(Game.getRessourcePathByName("ressources/SpaceShipType2.png"), 40, 40, Game.getHeight(), Game.getHeight());
 		}
 		
